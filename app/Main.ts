9a2cd3204b2eeffb6bbeb6f8284da8fc996766b0
rouanis5 <ouanis42@gmail.com>
2023-12-02 18:49:33 +0100

    reset project

--- app/Main.ts
+++ app/Main.ts
@@ -1,6 +1,3 @@
-import { WriterService } from './Writer/CodeWriter'
-import { ParserService } from './Parser/ParserService'
-
 export class Main {
-  constructor(private parser: ParserService, private writer: WriterService) {}
+  constructor() {}
 }
--- app/Writer/CodeWriter.ts
+++
@@ -1,9 +0,0 @@
-import { COMMAND_TYPE } from '../common/enums'
-
-export class WriterService {
-  constructor() {}
-
-  writeArithmetic(command: string) {}
-  writePushPop(command: COMMAND_TYPE) {}
-  close() {}
-}
